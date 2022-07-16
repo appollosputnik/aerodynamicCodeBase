5ad1c574ca513ea257e5cf6db07096d810e44bff
appollosputnik <yellowlemontree0821@gmail.com>
2022-07-16 19:19:56 +0530

    new push

---
+++ CAirfoilDesignerWindow.cpp
@@ -0,0 +1,345 @@
+#include "CAirfoilDesignerWindow.h"
+#include <fstream>
+#include <QVector2D>
+#include <QVector3D>
+#include "math.h"
+
+float pi = 4*atan(1.0);
+
+using namespace  std;
+
+struct VertexData
+{
+    QVector3D position;
+    QVector2D texCoord;
+};
+
+#include <QMouseEvent>
+
+#include <math.h>
+
+//INTERNATIONAL STANDARD WEATHER PREDICTOR
+//AERODYNAMIC DESIGNER & FLOW PREDICTOR
+//AERODYNAMIC POST PROCESSING ANALYZER
+//LUNAR MISSION DESIGNER
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+//Lunar Mission:
+1.  Design Lunar Vehicle (Voyger)
+2.  Design Path (Trajectory)
+3.  Landing on Moon
+4.  Rover for driving on Moon
+5.  Finding water (probe) and row Trees
+6.  Electronics Associated.
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+
+Aerofoil Geoometry
+   a. Draw a symmetric aerofoil with le(0.0) and te(100.0)
+   b. maximum thickness position (30.0, 0.0)
+   c. Bezier control points (0.0,0.0), (30.0, 15.0), (100.0, 0.0), (30.0, -15.0), (0.0, 0.0)
+   d. x, y. B(t) = SIGMA(i=0, i=n). nCi. (1-t)^(n-i). t^i. Pi
+   e. get aerofoilKnotList.
+   f. get dy/dx aerofoilKnotList
+   g. get d^2y/dx^2 aerofoilKnotList
+   h. camber=> maximum camber(30.0, 0.0) = 0.05C
+   i. Leading edge radius/curvature <= d^2y/dx^2    write option to modify
+   j. Trailing edge cusp (Kutaa condition) => dy/dx write option to modify
+
+   //Algoritm...
+   float xLE = 0.0; yLE = 0.0; xTE = 100.0; yTE = 0.0;
+   float maxThicknessPosX = 30.0, maxThicknessPosY = 0.0;
+   int nBzrCtrlPts = 5;
+   float nezierCtrlPosX[5] = {0.0, 30.0, 100.0, 30.0, 0.0};
+   float nezierCtrlPosY[5] = {0.0, 15.0, 0.0, -15.0, 0.0};
+   int nKnots = 100;
+   float ctrlPts_DyDx[4]; ctrlPts_D2yDx2[3];
+   float camber = 5.0;
+   float maxThickness = 30.0;
+   float DyDxTECuspBezrDyDx[m]; //smoothen between first and last points at the Trailing Edge...
+
+   */
+
+static bool ifDraw = false;
+static bool ifDrawKnots = false;
+static bool ifDrawHGrid = false;
+CAirfoilDesigner::CAirfoilDesigner(QWidget *parent) :
+    QGLWidget(parent),
+    xMin(-5.0),
+    xMax(5.0),
+    yMin(-5.0),
+    yMax(5.0)
+{
+    _cx = 0.0;
+    _cy = 0.0;
+    _cz = 0.0;
+    trans[0] = 0.0;
+    trans[1] = 0.0;
+    trans[2] = 0.0;
+    m_rot[0] = 0.0;
+    m_rot[1] = 0.0;
+    m_rot[2] = 0.0;
+    zoomFactor = 1.0;
+    nHGrid = 11;
+}
+
+CAirfoilDesigner::~CAirfoilDesigner()
+{
+}
+
+void CAirfoilDesigner::generateGridOverAirfoil()
+{
+    float *chi = new float[nHGrid], *eta = new float[nHGrid];
+
+        float inter = 1.0 / (nHGrid-1);
+
+        //out << "chi " << " eta " << " x " << " y " << "\n";
+
+        chi[0] = 0.0, eta[0] = 0.0;
+
+        for (int i = 1; i < nHGrid; i++)
+        {
+            chi[i] = chi[i - 1] + inter;
+            eta[i] = eta[i - 1] + inter;
+        }
+
+
+        xHyperbolic = new float[nHGrid], yHyperbolic = new float[nHGrid];
+
+        //out << 11 << "\n";
+        xHyperbolic[0] = 0.0, yHyperbolic[0] = 0.0;
+        for (int i = 1; i < nHGrid; i++)
+        {
+            yHyperbolic[i] = yHyperbolic[i - 1] + sqrt(1 + pow(((eta[i] - eta[i - 1]) / (chi[i] - chi[i - 1])), 2));
+            xHyperbolic[i] = (2 * xHyperbolic[i - 1] + sqrt(2 * xHyperbolic[i - 1] * 2 * xHyperbolic[i - 1] - 4 * ((xHyperbolic[i - 1] * xHyperbolic[i - 1]) - (yHyperbolic[i] - yHyperbolic[i - 1]) * (yHyperbolic[i] - yHyperbolic[i - 1]))))/2;
+        }
+        ifDrawHGrid = true;
+
+        float *x = new float[nHGrid];
+        float *y = new float[nHGrid];
+
+        for(int i=0; i<nHGrid; i++) {
+            x[i] = xHyperbolic[i];
+            y[i] = yHyperbolic[i];
+        }
+
+        xMin = x[0]; xMax = x[nHGrid-1];
+        yMin = y[0]; yMax = y[nHGrid-1];
+
+
+        _cx = xMin + ((xMax - xMin)/2.0);
+        _cy = yMin + ((yMax - yMin)/2.0);
+        _cz = 0.0;
+
+        updateGL();
+}
+
+void CAirfoilDesigner::mousePressEvent(QMouseEvent *e)
+{
+    // Save mouse press position
+    old_pos = e->pos();
+}
+
+void CAirfoilDesigner::mouseReleaseEvent(QMouseEvent *e)
+{
+}
+
+void CAirfoilDesigner::mouseMoveEvent(QMouseEvent *e)
+{
+    new_pos = e->pos();
+
+    dx = old_pos.x() - new_pos.x();
+    dy = new_pos.y() - old_pos.y();
+
+    if(e->button()==Qt::LeftButton)
+    {
+        trans[0] -= dx;
+        trans[1] -= dy;
+    }
+    else if(e->button()==Qt::RightButton)
+    {
+        m_rot[0] += dx;
+        m_rot[1] -= dy;
+        #define clamp(x) x = x > 360.0f ? x-360.0f : x < -360.0f ? x+=360.0f : x
+        clamp(m_rot[0]);
+        clamp(m_rot[1]);
+    }
+
+    else if(e->button()==Qt::MiddleButton)
+    {
+
+    }
+
+    else
+        QString str = "No operation at this point of time...";
+
+    old_pos = new_pos;
+
+    updateGL();
+}
+void CAirfoilDesigner::wheelEvent(QWheelEvent *e)
+{
+    if(e->delta() > 0)
+    {
+        zoomFactor /= 0.75;
+    }
+    else if(e->delta() <= 0)
+    {
+        zoomFactor *= 0.75;
+    }
+
+    updateGL();
+}
+
+void CAirfoilDesigner::initializeGL()
+{
+    glClearColor(0.0, 0.0, 0.0, 1.0); 
+}
+
+void CAirfoilDesigner::resizeGL(int w, int h)
+{
+    glViewport(0.0, 0.0, (GLsizei)w, (GLsizei)h);
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glOrtho(xMin-100, xMax+100, yMax+100, yMin-100, -5.0, 5.0);
+}
+
+void CAirfoilDesigner::paintGL()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glOrtho(xMin-100, xMax+100, yMax+100, yMin-100, -5.0, 5.0);
+
+    glTranslatef(trans[0], trans[1], 0.0f);
+    glTranslatef(_cx, _cy, _cz);
+    glScalef(zoomFactor, zoomFactor, zoomFactor);
+    glTranslatef(-_cx, -_cy, -_cz);
+
+/*  glBegin(GL_QUADS);
+    glPointSize(4.0);
+    glColor3f(0.0, 0.0, 1.0);
+    glVertex2f(xMin-3, yMin-3);
+    glVertex2f(xMax+3, yMin-3);
+    glVertex2f(xMax+3, yMax+3);
+    glVertex2f(xMin-3, yMax+3);
+    glEnd();
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); */
+
+    if(ifDraw) {
+    glBegin(GL_LINE_STRIP);
+    glColor3f(1.0, 0.0, 0.0);
+    for(int i=0; i<nAnchors; i++)
+    {
+        glVertex2f(_anchorXu[i], _anchorYu[i]);
+    }
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    glColor3f(1.0, 0.0, 0.0);
+    for(int i=0; i<nAnchors; i++)
+    {
+        glVertex2f(_anchorXl[i], _anchorYl[i]);
+    }
+    glEnd();
+
+    }
+
+    if(ifDrawKnots) {
+    glBegin(GL_LINE_STRIP);
+    glColor3f(0.0, 1.0, 0.0);
+    for(int i=0; i<nKnots; i++) {
+        glVertex2f(_knotsXu[i], _knotsYu[i]);
+    }
+    glEnd();
+
+    glBegin(GL_LINE_STRIP);
+    glColor3f(0.0, 1.0, 0.0);
+    for(int i=0; i<nKnots; i++) {
+        glVertex2f(_knotsXl[i], _knotsYl[i]);
+    }
+    glEnd();
+    }
+
+
+    //draw grid hyperbolic...
+    if(ifDrawHGrid)
+    {
+        glBegin(GL_LINE_STRIP);
+        glColor3f(0.0, 1.0, 0.0);
+        for(int i=0; i<nHGrid; i++) {
+            glVertex2f(xHyperbolic[i], yHyperbolic[i]);
+        }
+        glEnd();
+    }
+
+    glLoadIdentity();
+    glMatrixMode(GL_MODELVIEW);
+    glFlush();
+}
+
+void CAirfoilDesigner::set_anchors(int n, float *x1, float *y1, float *z1, float *x2, float *y2, float *z2)
+{
+     nAnchors = n;
+    _anchorXu = x1; _anchorYu = y1; _anchorZu = z1;
+    _anchorXl = x2; _anchorYl = y2; _anchorZl = z2;
+    if(!ifDraw)
+    ifDraw = !ifDraw;
+
+    float *x = new float[n*2];
+    int i = 0;
+    for(i=0; i<(n); i++)
+        x[i] = x1[i];
+    for(i=n; i<(n*2); i++)
+        x[i] = x2[i-n];
+
+    float *y = new float[n*2];
+    i = 0;
+    for(i=0; i<(n); i++)
+        y[i] = y1[i];
+    for(i=n; i<(n*2); i++)
+        y[i] = y2[i-n];
+
+    selectionSort(x, n);
+    selectionSort(y, n*2);
+
+    xMin = x[0]; xMax = x[n*2-1];
+    yMax = y[0]; yMin = y[n*2-1];
+
+    _cx = xMin + ((xMax - xMin)/2.0);
+    _cy = yMin + ((yMax - yMin)/2.0);
+    _cz = 0.0;
+    updateGL();
+}
+
+void CAirfoilDesigner::set_bezier_knots(int n, float *x1, float *y1, float *z1, float *x2, float *y2, float *z2)
+{
+     nKnots = n;
+    _knotsXu = x1; _knotsYu = y1; _knotsZu = z1;
+    _knotsXl = x2; _knotsYl = y2; _knotsZl = z2;
+    if(!ifDrawKnots)
+    ifDrawKnots = !ifDrawKnots;
+    updateGL();
+}
+
+void CAirfoilDesigner::selectionSort(float arr[], int size)
+{
+    int indexOfMin, pass, j;
+
+    for(pass=0; pass<size-1; pass++)
+    {
+        indexOfMin = pass;
+
+        for(j=pass+1; j<size; j++)
+            if(arr[j] < arr[indexOfMin])
+                indexOfMin = j;
+
+        std::swap(arr[pass], arr[indexOfMin]);
+    }
+}
---
+++ ui_dialogairfoildesigner.h
@@ -0,0 +1,358 @@
+/********************************************************************************
+** Form generated from reading UI file 'dialogairfoildesigner.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_DIALOGAIRFOILDESIGNER_H
+#define UI_DIALOGAIRFOILDESIGNER_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QHBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_DialogAirfoilDesigner
+{
+public:
+    QGridLayout *gridLayout_3;
+    QHBoxLayout *horizontalLayout;
+    QScrollArea *scrollArea_CONTROL_WIDGET;
+    QWidget *scrollAreaWidgetContents_2;
+    QGridLayout *gridLayout;
+    QVBoxLayout *verticalLayout_2;
+    QHBoxLayout *horizontalLayout_2;
+    QLabel *label;
+    QLineEdit *lineEdit_CHORD_LENGTH;
+    QSpacerItem *horizontalSpacer;
+    QHBoxLayout *horizontalLayout_3;
+    QLabel *label_2;
+    QLineEdit *lineEdit_CAMBER;
+    QSpacerItem *horizontalSpacer_2;
+    QHBoxLayout *horizontalLayout_4;
+    QLabel *label_3;
+    QLineEdit *lineEdit_MAX_THICKNESS;
+    QSpacerItem *horizontalSpacer_3;
+    QHBoxLayout *horizontalLayout_9;
+    QLabel *label_6;
+    QLineEdit *lineEdit_MAX_THICKNESS_POS;
+    QSpacerItem *horizontalSpacer_8;
+    QHBoxLayout *horizontalLayout_5;
+    QLabel *label_4;
+    QLineEdit *lineEdit_ORDER;
+    QSpacerItem *horizontalSpacer_4;
+    QHBoxLayout *horizontalLayout_6;
+    QLabel *label_5;
+    QLineEdit *lineEdit_NO_KNOTS;
+    QSpacerItem *horizontalSpacer_5;
+    QHBoxLayout *horizontalLayout_8;
+    QPushButton *pushButton_READ_ANCHORS;
+    QPushButton *pushButton_APPLY_READ_ANCHORS;
+    QSpacerItem *horizontalSpacer_7;
+    QHBoxLayout *horizontalLayout_7;
+    QPushButton *pushButton_APPLY_FULL;
+    QPushButton *pushButton_CANCEL;
+    QPushButton *pushButton_CLOSE;
+    QSpacerItem *horizontalSpacer_6;
+    QHBoxLayout *horizontalLayout_10;
+    QPushButton *pushButton_DRAW_H_GRID;
+    QSpacerItem *horizontalSpacer_9;
+    QSpacerItem *verticalSpacer_2;
+    QWidget *widget_SCREEN;
+    QGridLayout *gridLayout_2;
+    QVBoxLayout *verticalLayout;
+    QScrollArea *scrollArea_GRAPHICS_WINDOW;
+    QWidget *scrollAreaWidgetContents;
+    QTextEdit *textEdit_CONSOLE_WINDOW;
+
+    void setupUi(QDialog *DialogAirfoilDesigner)
+    {
+        if (DialogAirfoilDesigner->objectName().isEmpty())
+            DialogAirfoilDesigner->setObjectName(QString::fromUtf8("DialogAirfoilDesigner"));
+        DialogAirfoilDesigner->resize(831, 467);
+        gridLayout_3 = new QGridLayout(DialogAirfoilDesigner);
+        gridLayout_3->setObjectName(QString::fromUtf8("gridLayout_3"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        horizontalLayout->setContentsMargins(5, -1, 5, -1);
+        scrollArea_CONTROL_WIDGET = new QScrollArea(DialogAirfoilDesigner);
+        scrollArea_CONTROL_WIDGET->setObjectName(QString::fromUtf8("scrollArea_CONTROL_WIDGET"));
+        scrollArea_CONTROL_WIDGET->setWidgetResizable(true);
+        scrollAreaWidgetContents_2 = new QWidget();
+        scrollAreaWidgetContents_2->setObjectName(QString::fromUtf8("scrollAreaWidgetContents_2"));
+        scrollAreaWidgetContents_2->setGeometry(QRect(0, 0, 397, 445));
+        gridLayout = new QGridLayout(scrollAreaWidgetContents_2);
+        gridLayout->setObjectName(QString::fromUtf8("gridLayout"));
+        verticalLayout_2 = new QVBoxLayout();
+        verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
+        verticalLayout_2->setContentsMargins(-1, 5, -1, 5);
+        horizontalLayout_2 = new QHBoxLayout();
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout_2->setContentsMargins(5, -1, 5, -1);
+        label = new QLabel(scrollAreaWidgetContents_2);
+        label->setObjectName(QString::fromUtf8("label"));
+
+        horizontalLayout_2->addWidget(label);
+
+        lineEdit_CHORD_LENGTH = new QLineEdit(scrollAreaWidgetContents_2);
+        lineEdit_CHORD_LENGTH->setObjectName(QString::fromUtf8("lineEdit_CHORD_LENGTH"));
+
+        horizontalLayout_2->addWidget(lineEdit_CHORD_LENGTH);
+
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_2->addItem(horizontalSpacer);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_2);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setContentsMargins(5, -1, 5, -1);
+        label_2 = new QLabel(scrollAreaWidgetContents_2);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+
+        horizontalLayout_3->addWidget(label_2);
+
+        lineEdit_CAMBER = new QLineEdit(scrollAreaWidgetContents_2);
+        lineEdit_CAMBER->setObjectName(QString::fromUtf8("lineEdit_CAMBER"));
+
+        horizontalLayout_3->addWidget(lineEdit_CAMBER);
+
+        horizontalSpacer_2 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer_2);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_3);
+
+        horizontalLayout_4 = new QHBoxLayout();
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(5, -1, 5, -1);
+        label_3 = new QLabel(scrollAreaWidgetContents_2);
+        label_3->setObjectName(QString::fromUtf8("label_3"));
+
+        horizontalLayout_4->addWidget(label_3);
+
+        lineEdit_MAX_THICKNESS = new QLineEdit(scrollAreaWidgetContents_2);
+        lineEdit_MAX_THICKNESS->setObjectName(QString::fromUtf8("lineEdit_MAX_THICKNESS"));
+
+        horizontalLayout_4->addWidget(lineEdit_MAX_THICKNESS);
+
+        horizontalSpacer_3 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_4->addItem(horizontalSpacer_3);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_4);
+
+        horizontalLayout_9 = new QHBoxLayout();
+        horizontalLayout_9->setObjectName(QString::fromUtf8("horizontalLayout_9"));
+        label_6 = new QLabel(scrollAreaWidgetContents_2);
+        label_6->setObjectName(QString::fromUtf8("label_6"));
+
+        horizontalLayout_9->addWidget(label_6);
+
+        lineEdit_MAX_THICKNESS_POS = new QLineEdit(scrollAreaWidgetContents_2);
+        lineEdit_MAX_THICKNESS_POS->setObjectName(QString::fromUtf8("lineEdit_MAX_THICKNESS_POS"));
+
+        horizontalLayout_9->addWidget(lineEdit_MAX_THICKNESS_POS);
+
+        horizontalSpacer_8 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_9->addItem(horizontalSpacer_8);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_9);
+
+        horizontalLayout_5 = new QHBoxLayout();
+        horizontalLayout_5->setObjectName(QString::fromUtf8("horizontalLayout_5"));
+        horizontalLayout_5->setContentsMargins(5, -1, 5, -1);
+        label_4 = new QLabel(scrollAreaWidgetContents_2);
+        label_4->setObjectName(QString::fromUtf8("label_4"));
+
+        horizontalLayout_5->addWidget(label_4);
+
+        lineEdit_ORDER = new QLineEdit(scrollAreaWidgetContents_2);
+        lineEdit_ORDER->setObjectName(QString::fromUtf8("lineEdit_ORDER"));
+
+        horizontalLayout_5->addWidget(lineEdit_ORDER);
+
+        horizontalSpacer_4 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_5->addItem(horizontalSpacer_4);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_5);
+
+        horizontalLayout_6 = new QHBoxLayout();
+        horizontalLayout_6->setObjectName(QString::fromUtf8("horizontalLayout_6"));
+        horizontalLayout_6->setContentsMargins(5, -1, 5, -1);
+        label_5 = new QLabel(scrollAreaWidgetContents_2);
+        label_5->setObjectName(QString::fromUtf8("label_5"));
+
+        horizontalLayout_6->addWidget(label_5);
+
+        lineEdit_NO_KNOTS = new QLineEdit(scrollAreaWidgetContents_2);
+        lineEdit_NO_KNOTS->setObjectName(QString::fromUtf8("lineEdit_NO_KNOTS"));
+
+        horizontalLayout_6->addWidget(lineEdit_NO_KNOTS);
+
+        horizontalSpacer_5 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_6->addItem(horizontalSpacer_5);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_6);
+
+        horizontalLayout_8 = new QHBoxLayout();
+        horizontalLayout_8->setObjectName(QString::fromUtf8("horizontalLayout_8"));
+        pushButton_READ_ANCHORS = new QPushButton(scrollAreaWidgetContents_2);
+        pushButton_READ_ANCHORS->setObjectName(QString::fromUtf8("pushButton_READ_ANCHORS"));
+
+        horizontalLayout_8->addWidget(pushButton_READ_ANCHORS);
+
+        pushButton_APPLY_READ_ANCHORS = new QPushButton(scrollAreaWidgetContents_2);
+        pushButton_APPLY_READ_ANCHORS->setObjectName(QString::fromUtf8("pushButton_APPLY_READ_ANCHORS"));
+
+        horizontalLayout_8->addWidget(pushButton_APPLY_READ_ANCHORS);
+
+        horizontalSpacer_7 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_8->addItem(horizontalSpacer_7);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_8);
+
+        horizontalLayout_7 = new QHBoxLayout();
+        horizontalLayout_7->setObjectName(QString::fromUtf8("horizontalLayout_7"));
+        pushButton_APPLY_FULL = new QPushButton(scrollAreaWidgetContents_2);
+        pushButton_APPLY_FULL->setObjectName(QString::fromUtf8("pushButton_APPLY_FULL"));
+
+        horizontalLayout_7->addWidget(pushButton_APPLY_FULL);
+
+        pushButton_CANCEL = new QPushButton(scrollAreaWidgetContents_2);
+        pushButton_CANCEL->setObjectName(QString::fromUtf8("pushButton_CANCEL"));
+
+        horizontalLayout_7->addWidget(pushButton_CANCEL);
+
+        pushButton_CLOSE = new QPushButton(scrollAreaWidgetContents_2);
+        pushButton_CLOSE->setObjectName(QString::fromUtf8("pushButton_CLOSE"));
+
+        horizontalLayout_7->addWidget(pushButton_CLOSE);
+
+        horizontalSpacer_6 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_7->addItem(horizontalSpacer_6);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_7);
+
+        horizontalLayout_10 = new QHBoxLayout();
+        horizontalLayout_10->setObjectName(QString::fromUtf8("horizontalLayout_10"));
+        horizontalLayout_10->setContentsMargins(5, -1, 5, -1);
+        pushButton_DRAW_H_GRID = new QPushButton(scrollAreaWidgetContents_2);
+        pushButton_DRAW_H_GRID->setObjectName(QString::fromUtf8("pushButton_DRAW_H_GRID"));
+
+        horizontalLayout_10->addWidget(pushButton_DRAW_H_GRID);
+
+        horizontalSpacer_9 = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_10->addItem(horizontalSpacer_9);
+
+
+        verticalLayout_2->addLayout(horizontalLayout_10);
+
+        verticalSpacer_2 = new QSpacerItem(20, 40, QSizePolicy::Minimum, QSizePolicy::Expanding);
+
+        verticalLayout_2->addItem(verticalSpacer_2);
+
+
+        gridLayout->addLayout(verticalLayout_2, 0, 0, 1, 1);
+
+        scrollArea_CONTROL_WIDGET->setWidget(scrollAreaWidgetContents_2);
+
+        horizontalLayout->addWidget(scrollArea_CONTROL_WIDGET);
+
+        widget_SCREEN = new QWidget(DialogAirfoilDesigner);
+        widget_SCREEN->setObjectName(QString::fromUtf8("widget_SCREEN"));
+        gridLayout_2 = new QGridLayout(widget_SCREEN);
+        gridLayout_2->setObjectName(QString::fromUtf8("gridLayout_2"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setContentsMargins(-1, 5, -1, 5);
+        scrollArea_GRAPHICS_WINDOW = new QScrollArea(widget_SCREEN);
+        scrollArea_GRAPHICS_WINDOW->setObjectName(QString::fromUtf8("scrollArea_GRAPHICS_WINDOW"));
+        scrollArea_GRAPHICS_WINDOW->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 376, 205));
+        scrollArea_GRAPHICS_WINDOW->setWidget(scrollAreaWidgetContents);
+
+        verticalLayout->addWidget(scrollArea_GRAPHICS_WINDOW);
+
+        textEdit_CONSOLE_WINDOW = new QTextEdit(widget_SCREEN);
+        textEdit_CONSOLE_WINDOW->setObjectName(QString::fromUtf8("textEdit_CONSOLE_WINDOW"));
+
+        verticalLayout->addWidget(textEdit_CONSOLE_WINDOW);
+
+
+        gridLayout_2->addLayout(verticalLayout, 0, 0, 1, 1);
+
+
+        horizontalLayout->addWidget(widget_SCREEN);
+
+
+        gridLayout_3->addLayout(horizontalLayout, 0, 0, 1, 1);
+
+
+        retranslateUi(DialogAirfoilDesigner);
+
+        QMetaObject::connectSlotsByName(DialogAirfoilDesigner);
+    } // setupUi
+
+    void retranslateUi(QDialog *DialogAirfoilDesigner)
+    {
+        DialogAirfoilDesigner->setWindowTitle(QCoreApplication::translate("DialogAirfoilDesigner", "Dialog", nullptr));
+        label->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Chord Length :", nullptr));
+        lineEdit_CHORD_LENGTH->setText(QCoreApplication::translate("DialogAirfoilDesigner", "100", nullptr));
+        label_2->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Camber (%c):", nullptr));
+        lineEdit_CAMBER->setText(QCoreApplication::translate("DialogAirfoilDesigner", "7", nullptr));
+        label_3->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Max Thickness (%c):", nullptr));
+        lineEdit_MAX_THICKNESS->setText(QCoreApplication::translate("DialogAirfoilDesigner", "30", nullptr));
+        label_6->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Max Thickness position:", nullptr));
+        lineEdit_MAX_THICKNESS_POS->setText(QCoreApplication::translate("DialogAirfoilDesigner", "30", nullptr));
+        label_4->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Order:", nullptr));
+        lineEdit_ORDER->setText(QCoreApplication::translate("DialogAirfoilDesigner", "2", nullptr));
+        label_5->setText(QCoreApplication::translate("DialogAirfoilDesigner", "No. Knots:", nullptr));
+        lineEdit_NO_KNOTS->setText(QCoreApplication::translate("DialogAirfoilDesigner", "11", nullptr));
+        pushButton_READ_ANCHORS->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Read Anchors", nullptr));
+        pushButton_APPLY_READ_ANCHORS->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Apply", nullptr));
+        pushButton_APPLY_FULL->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Apply", nullptr));
+        pushButton_CANCEL->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Cancel", nullptr));
+        pushButton_CLOSE->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Close", nullptr));
+        pushButton_DRAW_H_GRID->setText(QCoreApplication::translate("DialogAirfoilDesigner", "Draw Hyperbolic Grid...", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class DialogAirfoilDesigner: public Ui_DialogAirfoilDesigner {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_DIALOGAIRFOILDESIGNER_H
